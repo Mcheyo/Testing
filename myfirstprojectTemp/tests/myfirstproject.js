5b32650fa721fcfc68bc23ec37d0cced7cec3080
mcheyo <mcheyo.mc@gmail.com>
2022-10-04 14:19:28 -0400

    Temp name change

---
+++ myfirstprojectTemp/tests/myfirstproject.js
@@ -0,0 +1,52 @@
+const anchor = require('@project-serum/anchor')
+
+const { SystemProgram} = anchor.web3; 
+
+const main = async() => { 
+
+    console.log("Starting test...")
+
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider); 
+
+    const program = anchor.workspace.Myfirstproject; 
+
+    const baseAccount = anchor.web3.Keypair.generate()
+
+
+    const tx = await program.rpc.startStuffOff({ 
+
+      accounts: { 
+        baseAccount: baseAccount.publicKey, 
+        user: provider.wallet.publicKey, 
+        systemProgram: SystemProgram.programId,
+      }, 
+      signers: [baseAccount],
+    })
+
+    console.log("Your transaction signature", tx)
+
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+     console.log('👀 GIF Count', account.totalGifs.toString())
+
+   await program.rpc.addGif({
+    accounts: { 
+      baseAccount: baseAccount.publicKey,
+    },
+   });
+
+   account = await program.account.baseAccount.fetch(baseAccount.publicKey); 
+   console.log(' GIF Count', account.totalGifs.toString())
+}
+
+const runMain = async () => {
+    try {
+      await main()
+      process.exit(0)
+    } catch (error) {
+      console.error(error)
+      process.exit(1)
+    }
+  }
+
+  runMain()
